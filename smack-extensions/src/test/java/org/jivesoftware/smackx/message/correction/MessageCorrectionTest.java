adfe07f4d8892fa0c08164ed786b55c4d5bd2941
ramabit <f.e.ramirez94@gmail.com>
2016-04-05 16:05:42 -0300

    adding license documentation

--- smack-extensions/src/test/java/org/jivesoftware/smackx/message/correction/MessageCorrectionTest.java
+++ smack-extensions/src/test/java/org/jivesoftware/smackx/message/correction/MessageCorrectionTest.java
@@ -1,3 +1,19 @@
+/**
+ *
+ * Copyright 2016 Fernando Ramirez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jivesoftware.smackx.message.correction;
 
 import org.jivesoftware.smack.packet.Message;
@@ -13,9 +29,9 @@ public class MessageCorrectionTest {
 	private final String jidInitialMessage = "bad1";
    
 	private final String initialMessageXml = 
-					"<message to='juliet@capulet.net/balcony' id='good1'>"
-					+ "<body>But soft, what light through yonder window breaks?</body>"
-					+ "</message>";
+			"<message to='juliet@capulet.net/balcony' id='good1'>"
+			+ "<body>But soft, what light through yonder window breaks?</body>"
+			+ "</message>";
 	
 	private final CharSequence messageCorrectionXml = "<replace id='bad1' xmlns='urn:xmpp:message-correct:0'/>";
 	
@@ -25,7 +41,6 @@ public class MessageCorrectionTest {
 			+ "<replace id='bad1' xmlns='urn:xmpp:message-correct:0'/>"
 			+ "</message>";    
 	
-
 	@Before
 	public void setStanzas() throws Exception{
         initialMessage = (Message) PacketParserUtils.parseStanza(initialMessageXml);
