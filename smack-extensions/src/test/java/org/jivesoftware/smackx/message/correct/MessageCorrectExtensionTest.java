97a15888763ef47b6799d0ab1fd33517c7cf2ac3
ramabit <f.e.ramirez94@gmail.com>
2016-04-07 12:15:25 -0300

    add javadoc

--- smack-extensions/src/test/java/org/jivesoftware/smackx/message/correct/MessageCorrectExtensionTest.java
+++ smack-extensions/src/test/java/org/jivesoftware/smackx/message/correct/MessageCorrectExtensionTest.java
@@ -13,6 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * @author Fernando Ramirez, f.e.ramirez94@gmail.com
  */
 package org.jivesoftware.smackx.message.correct;
 
