2ce91bbaab01d5bf260ffd9c718bb98303bf6689
ramabit <f.e.ramirez94@gmail.com>
2016-04-11 14:33:06 -0300

    fix documentation

--- smack-extensions/src/main/java/org/jivesoftware/smackx/message_correct/provider/MessageCorrectProvider.java
+++ smack-extensions/src/main/java/org/jivesoftware/smackx/message_correct/provider/MessageCorrectProvider.java
@@ -26,7 +26,7 @@ import org.xmlpull.v1.XmlPullParserException;
 
 /**
  * A ExtensionElementProvider for the MessageCorrectExtension. As
- * MessageCorrection elements have only the JID of the message to replace.
+ * MessageCorrection elements have only the ID of the message to replace.
  * 
  * @author Fernando Ramirez, f.e.ramirez94@gmail.com
  */
