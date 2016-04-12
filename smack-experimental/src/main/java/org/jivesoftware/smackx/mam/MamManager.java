40bf8f589cbae5b6ab232fefc8c76ce895c974c3
ramabit <f.e.ramirez94@gmail.com>
2016-04-12 17:50:57 -0300

    first attempt of mam implementation

---
+++ smack-experimental/src/main/java/org/jivesoftware/smackx/mam/MamManager.java
@@ -0,0 +1,207 @@
+/**
+ *
+ * Copyright 2016 Fernando Ramirez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jivesoftware.smackx.mam;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.WeakHashMap;
+
+import org.jivesoftware.smack.ConnectionCreationListener;
+import org.jivesoftware.smack.Manager;
+import org.jivesoftware.smack.PacketCollector;
+import org.jivesoftware.smack.SmackException.NoResponseException;
+import org.jivesoftware.smack.SmackException.NotConnectedException;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPConnectionRegistry;
+import org.jivesoftware.smack.XMPPException.XMPPErrorException;
+import org.jivesoftware.smack.packet.IQ;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smackx.disco.ServiceDiscoveryManager;
+import org.jivesoftware.smackx.forward.packet.Forwarded;
+import org.jivesoftware.smackx.mam.filter.MamMessageFinFilter;
+import org.jivesoftware.smackx.mam.filter.MamMessageResultFilter;
+import org.jivesoftware.smackx.mam.packet.MamPacket;
+import org.jivesoftware.smackx.mam.packet.MamPacket.MamFinExtension;
+import org.jivesoftware.smackx.mam.packet.MamQueryIQ;
+import org.jivesoftware.smackx.mam.packet.MamPacket.MamResultExtension;
+import org.jivesoftware.smackx.rsm.packet.RSMSet;
+import org.jivesoftware.smackx.xdata.FormField;
+import org.jivesoftware.smackx.xdata.packet.DataForm;
+import org.jxmpp.util.XmppDateTime;
+
+/**
+ * Message Archive Management Manager class.
+ * 
+ * @see <a href="http://xmpp.org/extensions/xep-0313.html">XEP-0313: Message
+ *      Archive Management</a>
+ *
+ */
+public final class MamManager extends Manager {
+
+    static {
+        XMPPConnectionRegistry.addConnectionCreationListener(new ConnectionCreationListener() {
+            @Override
+            public void connectionCreated(XMPPConnection connection) {
+                getInstanceFor(connection);
+            }
+        });
+    }
+
+    private static final Map<XMPPConnection, MamManager> INSTANCES = new WeakHashMap<>();
+
+    public static synchronized MamManager getInstanceFor(XMPPConnection connection) {
+        MamManager mamManager = INSTANCES.get(connection);
+
+        if (mamManager == null) {
+            mamManager = new MamManager(connection);
+            INSTANCES.put(connection, mamManager);
+        }
+
+        return mamManager;
+    }
+
+    private MamManager(XMPPConnection connection) {
+        super(connection);
+        ServiceDiscoveryManager.getInstanceFor(connection).addFeature(MamPacket.NAMESPACE);
+    }
+
+    public MamQueryResult queryArchive(Integer max, Date start, Date end, String withJid)
+            throws NoResponseException, XMPPErrorException, NotConnectedException, InterruptedException {
+        DataForm dataForm = null;
+        String queryId = UUID.randomUUID().toString();
+
+        if (start != null || end != null || withJid != null) {
+            dataForm = getNewMamForm();
+
+            if (start != null) {
+                FormField formField = new FormField("start");
+                formField.addValue(XmppDateTime.formatXEP0082Date(start));
+                dataForm.addField(formField);
+            }
+            if (end != null) {
+                FormField formField = new FormField("end");
+                formField.addValue(XmppDateTime.formatXEP0082Date(end));
+                dataForm.addField(formField);
+            }
+            if (withJid != null) {
+                FormField formField = new FormField("with");
+                formField.addValue(withJid);
+                dataForm.addField(formField);
+            }
+        }
+
+        MamQueryIQ mamQueryIQ = new MamQueryIQ(queryId, dataForm);
+        mamQueryIQ.setType(IQ.Type.set);
+
+        if (max != null) {
+            RSMSet rsmSet = new RSMSet(max);
+            mamQueryIQ.addExtension(rsmSet);
+        }
+
+        return queryArchive(mamQueryIQ, 0);
+    }
+
+    public MamQueryResult pageNext(MamQueryResult mamQueryResult, int count)
+            throws NoResponseException, XMPPErrorException, NotConnectedException, InterruptedException {
+        RSMSet previousResultRsmSet = mamQueryResult.mamFin.getRSMSet();
+        RSMSet requestRsmSet = new RSMSet(count, previousResultRsmSet.getLast(), RSMSet.PageDirection.after);
+        return page(mamQueryResult, requestRsmSet);
+    }
+
+    public MamQueryResult page(MamQueryResult mamQueryResult, RSMSet rsmSet)
+            throws NoResponseException, XMPPErrorException, NotConnectedException, InterruptedException {
+        MamQueryIQ mamQueryIQ = new MamQueryIQ(UUID.randomUUID().toString(), mamQueryResult.form);
+        mamQueryIQ.setType(IQ.Type.set);
+        mamQueryIQ.addExtension(rsmSet);
+        return queryArchive(mamQueryIQ, 0);
+    }
+
+    private MamQueryResult queryArchive(MamQueryIQ mamQueryIq, long extraTimeout)
+            throws NoResponseException, XMPPErrorException, NotConnectedException, InterruptedException {
+        if (extraTimeout < 0) {
+            throw new IllegalArgumentException("extra timeout must be zero or positive");
+        }
+
+        final XMPPConnection connection = connection();
+        MamFinExtension mamFinExtension = null;
+
+        PacketCollector finMessageCollector = connection.createPacketCollector(new MamMessageFinFilter(mamQueryIq));
+
+        PacketCollector.Configuration resultCollectorConfiguration = PacketCollector.newConfiguration()
+                .setStanzaFilter(new MamMessageResultFilter(mamQueryIq)).setCollectorToReset(finMessageCollector);
+
+        PacketCollector resultCollector = connection.createPacketCollector(resultCollectorConfiguration);
+
+        try {
+            connection.createPacketCollectorAndSend(mamQueryIq).nextResultOrThrow();
+            Message mamFinMessage = finMessageCollector
+                    .nextResultOrThrow(connection.getPacketReplyTimeout() + extraTimeout);
+            mamFinExtension = MamFinExtension.from(mamFinMessage);
+        } finally {
+            resultCollector.cancel();
+            finMessageCollector.cancel();
+        }
+
+        List<Forwarded> messages = new ArrayList<>(resultCollector.getCollectedCount());
+
+        for (Message resultMessage = resultCollector.pollResult(); resultMessage != null;) {
+            // XEP-313 § 4.2
+            MamResultExtension mamResultExtension = MamResultExtension.from(resultMessage);
+            messages.add(mamResultExtension.getForwarded());
+        }
+
+        return new MamQueryResult(messages, mamFinExtension, DataForm.from(mamQueryIq));
+    }
+
+    public final static class MamQueryResult {
+        public final List<Forwarded> messages;
+        public final MamFinExtension mamFin;
+        private final DataForm form;
+
+        private MamQueryResult(List<Forwarded> messages, MamFinExtension mamFin, DataForm form) {
+            this.messages = messages;
+            this.mamFin = mamFin;
+            this.form = form;
+        }
+    }
+
+    /**
+     * Returns true if Message Archive Management is supported by the server.
+     * 
+     * @return true if Message ARchive Management is supported by the server.
+     * @throws NotConnectedException
+     * @throws XMPPErrorException
+     * @throws NoResponseException
+     * @throws InterruptedException
+     */
+    public boolean isSupportedByServer()
+            throws NoResponseException, XMPPErrorException, NotConnectedException, InterruptedException {
+        return ServiceDiscoveryManager.getInstanceFor(connection()).serverSupportsFeature(MamPacket.NAMESPACE);
+    }
+
+    private static DataForm getNewMamForm() {
+        FormField field = new FormField(FormField.FORM_TYPE);
+        field.setType(FormField.Type.hidden);
+        field.addValue(MamPacket.NAMESPACE);
+        DataForm form = new DataForm(DataForm.Type.submit);
+        form.addField(field);
+        return form;
+    }
+}
---
+++ smack-experimental/src/main/java/org/jivesoftware/smackx/mam/packet/MamPacket.java
@@ -0,0 +1,154 @@
+/**
+ *
+ * Copyright 2016 Fernando Ramirez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jivesoftware.smackx.mam.packet;
+
+import org.jivesoftware.smack.packet.ExtensionElement;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.util.StringUtils;
+import org.jivesoftware.smack.util.XmlStringBuilder;
+import org.jivesoftware.smackx.forward.packet.Forwarded;
+import org.jivesoftware.smackx.rsm.packet.RSMSet;
+
+/**
+ * MAM packet class.
+ * 
+ * @see <a href="http://xmpp.org/extensions/xep-0313.html">XEP-0313: Message
+ *      Archive Management</a>
+ *
+ */
+public class MamPacket {
+
+    public static final String NAMESPACE = "urn:xmpp:mam:1";
+
+    public static abstract class AbstractMamExtension implements ExtensionElement {
+        public final String queryId;
+
+        protected AbstractMamExtension(String queryId) {
+            this.queryId = queryId;
+        }
+
+        public final String getQueryId() {
+            return queryId;
+        }
+
+        public final String getNamespace() {
+            return NAMESPACE;
+        }
+
+    }
+
+    public static class MamFinExtension extends AbstractMamExtension {
+
+        public static final String ELEMENT = "fin";
+
+        private final RSMSet rsmSet;
+        private final boolean complete;
+        private final boolean stable;
+
+        public MamFinExtension(String queryId, RSMSet rsmSet, boolean complete, boolean stable) {
+            super(queryId);
+            if (rsmSet == null) {
+                throw new IllegalArgumentException("rsmSet must not be null");
+            }
+            this.rsmSet = rsmSet;
+            this.complete = complete;
+            this.stable = stable;
+        }
+
+        public RSMSet getRSMSet() {
+            return rsmSet;
+        }
+
+        public boolean isComplete() {
+            return complete;
+        }
+
+        public boolean isStable() {
+            return stable;
+        }
+
+        @Override
+        public String getElementName() {
+            return ELEMENT;
+        }
+
+        @Override
+        public XmlStringBuilder toXML() {
+            XmlStringBuilder xml = new XmlStringBuilder();
+            xml.halfOpenElement(this);
+            xml.optAttribute("queryid", queryId);
+            xml.optBooleanAttribute("complete", complete);
+            xml.optBooleanAttribute("stable", stable);
+            if (rsmSet == null) {
+                xml.closeEmptyElement();
+            } else {
+                xml.rightAngleBracket();
+                xml.element(rsmSet);
+                xml.closeElement(this);
+            }
+            return xml;
+        }
+
+        public static MamFinExtension from(Message message) {
+            return message.getExtension(ELEMENT, NAMESPACE);
+        }
+    }
+
+    public static class MamResultExtension extends AbstractMamExtension {
+
+        public static final String ELEMENT = "result";
+
+        private final String id;
+        private final Forwarded forwarded;
+
+        public MamResultExtension(String queryId, String id, Forwarded forwarded) {
+            super(queryId);
+            if (StringUtils.isNotEmpty(id)) {
+                throw new IllegalArgumentException("id must not be null or empty");
+            }
+            if (forwarded == null) {
+                throw new IllegalArgumentException("forwarded must no be null");
+            }
+            this.id = id;
+            this.forwarded = forwarded;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public Forwarded getForwarded() {
+            return forwarded;
+        }
+
+        @Override
+        public String getElementName() {
+            return ELEMENT;
+        }
+
+        @Override
+        public CharSequence toXML() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public static MamResultExtension from(Message message) {
+            return message.getExtension(ELEMENT, NAMESPACE);
+        }
+
+    }
+}
