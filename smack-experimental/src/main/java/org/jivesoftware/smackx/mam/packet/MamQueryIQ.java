6e96cdadfe3214ae90d2cdbd50305b309220b138
ramabit <f.e.ramirez94@gmail.com>
2016-04-22 11:12:15 -0300

    fix author

--- smack-experimental/src/main/java/org/jivesoftware/smackx/mam/packet/MamQueryIQ.java
+++ smack-experimental/src/main/java/org/jivesoftware/smackx/mam/packet/MamQueryIQ.java
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright 2016 Fernando Ramirez
+ * Copyright © 2015 Florian Schmaus
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
