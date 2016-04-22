513414e04bb2b3eae3fa5e8845d731b5de66a453
ramabit <f.e.ramirez94@gmail.com>
2016-04-22 17:45:51 -0300

    add test for query archive

---
+++ smack-experimental/src/test/java/org/jivesoftware/smackx/mam/QueryArchiveTest.java
@@ -0,0 +1,68 @@
+/**
+ *
+ * Copyright 2016 Fernando Ramirez
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jivesoftware.smackx.mam;
+
+import static org.mockito.Mockito.mock;
+
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.packet.IQ;
+import org.jivesoftware.smackx.mam.packet.MamPacket;
+import org.jivesoftware.smackx.mam.packet.MamQueryIQ;
+import org.jivesoftware.smackx.xdata.FormField;
+import org.jivesoftware.smackx.xdata.packet.DataForm;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QueryArchiveTest {
+
+    private XMPPConnection connection;
+
+    @Before
+    public void setup() {
+        // mock connection
+        connection = mock(XMPPConnection.class);
+    }
+
+    private DataForm getNewMamForm() {
+        FormField field = new FormField(FormField.FORM_TYPE);
+        field.setType(FormField.Type.hidden);
+        field.addValue(MamPacket.NAMESPACE);
+        DataForm form = new DataForm(DataForm.Type.submit);
+        form.addField(field);
+        return form;
+    }
+
+    private String getMamSimpleQueryIQ(String stanzaId) {
+        return "<iq id='" + stanzaId + "' type='set'>"
+                + "<query xmlns='urn:xmpp:mam:1' queryid='testid'>"
+                + "<x xmlns='jabber:x:data' type='submit'>" + "<field var='FORM_TYPE' type='hidden'>"
+                + "<value>urn:xmpp:mam:1</value>" + "</field>" + "</x>" + "</query>" + "</iq>";
+    }
+
+    @Test
+    public void checkMamQueryIQ() throws Exception {
+        String queryId = "testid";
+        DataForm dataForm = getNewMamForm();
+
+        MamQueryIQ mamQueryIQ = new MamQueryIQ(queryId, dataForm);
+        mamQueryIQ.setType(IQ.Type.set);
+
+        Assert.assertEquals(mamQueryIQ.toString(), getMamSimpleQueryIQ(mamQueryIQ.getStanzaId()));
+    }
+
+}
